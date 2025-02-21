1e49b6408aa20f3a31aa2eae6a0cf6b395f30f55
Jason Hermann <986681+JasonHermann@users.noreply.github.com>
2025-02-21 13:59:47 -0500

    1.2.1 Keyed Alphabets

---
+++ Tests/foundation.tests/Permutations/KeyedPermutationTests.cs
@@ -0,0 +1,45 @@
+﻿using foundation.Permutations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foundation.tests.Permutations
+{
+    public class KeyedPermutationTests
+    {
+        const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        [Fact]
+        public void KeyedPermutation_HappyPath()
+        {
+            // Arrange
+            var key = "zebra";
+            var expectedPermutation = "zebracdfghijklmnopqstuvwxy";
+
+            // Act
+            string permutation = new([.. KeyedPermutation.PermuteByKey(alphabet, key)]);
+
+            // Assert
+            Assert.Equal(expectedPermutation, permutation);
+        }
+
+        /// <summary>
+        /// From 1.2.1 Keyed Alphabets of Modern Cryptanalysis
+        /// </summary>
+        [Fact]
+        public void KeyedPermutation_HappyPath_2()
+        {
+            // Arrange
+            var key = "swordfish";
+            var expectedPermutation = "swordfihabcegjklmnpqtuvxyz";
+
+            // Act
+            string permutation = new([.. KeyedPermutation.PermuteByKey(alphabet, key)]);
+
+            // Assert
+            Assert.Equal(expectedPermutation, permutation);
+        }
+    }
+}
---
+++ foundation/Permutations/KeyedPermutation.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foundation.Permutations
+{
+    /// <summary>
+    /// A keyed permutation is performed by returning source as key, less duplication and less
+    /// any keys not appearing in source.
+    /// 
+    /// Simplest case, the returned value is the key.
+    /// </summary>
+    public static class KeyedPermutation
+    {
+        public static IList<T> PermuteByKey<T>(IList<T> source, IList<T> key)
+        {
+            if (source == null) throw new ArgumentNullException();
+            if (key == null) throw new ArgumentNullException();
+
+            if (source.Count == 0) return [];
+
+            var sourceHash = new HashSet<T>();
+            for (int i = 0; i < source.Count; i++)
+            {
+                T e = source[i];
+                if (sourceHash.Add(e) == false)
+                    continue;
+            }
+
+            var output = new List<T>();
+            var hash = new HashSet<T>();
+            for (int i = 0; i < key.Count; i++)
+            {
+                T e = key[i];
+                // Ignore values in key that don't appear in alphabet.
+                if(sourceHash.Contains(e) == false)
+                    continue;
+
+                if (hash.Add(e) == false)
+                    continue;
+                output.Add(e);
+            }
+            for (int i = 0; i < source.Count; i++)
+            {
+                T e = source[i];
+                if (hash.Add(e) == false)
+                    continue;
+                output.Add(e);
+            }
+            return output;
+        }
+
+        public static IEnumerable<T> PermuteByKey<T>(IEnumerable<T> source, IEnumerable<T> key)
+        {
+            if (source == null) throw new ArgumentNullException();
+            if (key == null) throw new ArgumentNullException();
+
+            var sourceHash = new HashSet<T>();
+            foreach(var e in source)
+            {
+                if (sourceHash.Add(e) == false)
+                    continue;
+            }
+
+            var hash = new HashSet<T>();
+            foreach(var e in key)
+            {
+                // Ignore values in key that don't appear in alphabet.
+                if (sourceHash.Contains(e) == false)
+                    continue;
+
+                if (hash.Add(e) == false)
+                    continue;
+                yield return e;
+            }
+            foreach (var e in source)
+            {
+                if (hash.Add(e) == false)
+                    continue;
+                yield return e;
+            }
+            yield break;
+        }
+    }
+}
